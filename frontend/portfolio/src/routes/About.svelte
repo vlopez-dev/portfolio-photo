9109f06fdbe99197c305c09a478ed9c6e71cf40a
vlopez <victorl_222@hotmail.com>
2023-07-25 21:34:01 -0300

    add title and subtitle about

--- frontend/portfolio/src/routes/About.svelte
+++ frontend/portfolio/src/routes/About.svelte
@@ -1,16 +1,21 @@
 
 
 <section class="section">
-<div class="columns is-multiline mx-2 my-6" style="outline: 2px solid green;">
+<div class="columns is-multiline mx-2 my-6" >
     
-    <div class="column" style="outline: 2px solid red;"></div>
-    <div class="column is-one-third" style="outline: 2px solid red;">
-        <figure class="is-flex is-justify-content-center " style="outline: 2px solid green;">
+    <div class="column" ></div>
+    <div class="column is-one-third" >
+        <figure class="is-flex is-justify-content-center ">
         <img class="is-rounded about-image-shadow" src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=699&q=80" alt="">
     </figure>
 </div>
-    <div class="column" style="outline: 2px solid blue;"></div>
-    <div class="column" style="outline: 2px solid blue;"></div>
+    <div class="column" >
+        <div class="container">
+            <h2 class="title">Titulo</h2>
+            <p class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aut voluptate inventore placeat odio perferendis accusamus repellat? Nihil, accusantium reprehenderit, voluptates sequi, aspernatur laudantium vero labore fuga in quibusdam cumque!</p>
+        </div>
+    </div>
+    <div class="column" ></div>
 
 </div>
 </section>
