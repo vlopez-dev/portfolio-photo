7d5716e8659db80a8a678dec89ca354601d1aff2
vlopez <victorl_222@hotmail.com>
2023-07-31 21:51:52 -0300

    lightbox gallery test

--- frontend/portfolio/src/routes/Gallery.svelte
+++ frontend/portfolio/src/routes/Gallery.svelte
@@ -5,12 +5,12 @@
 	}
 </script>
 <section class="section">
-    <div class="container">
-      <h2 class="title title-section">Photo</h2>
-      <div class="columns is-multiline">
-          <div class="column is-4">
-            <div class="gallery-item box" data-index="">
-              <figure class="image is-square">
+    <div class="container is-fluid">
+      <!-- <h2 class="title title-section">Photo</h2> -->
+      <div class="columns ">
+          <div class="column">
+            <div class="gallery-item " data-index="">
+              <figure class="image ">
                 <img src="https://images.unsplash.com/photo-1689776574086-50ff04ae5dec?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="" />
                 <div class="album-info">
                   <h3>Nombre del álbum 1</h3>
@@ -19,10 +19,10 @@
               </figure>
             </div>
           </div>
-          <div class="column is-4">
-            <div class="gallery-item box" data-index="">
-              <figure class="image is-square">
-                <img src="https://images.unsplash.com/photo-1545341425-ee53bbfd2653?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="" />
+          <div class="column">
+            <div class="gallery-item " data-index="">
+              <figure class="image ">
+                <img src="../../img/rob-potter-hjIBz9JeS3I-unsplash.jpg" alt="" />
                 <div class="album-info">
                   <h3>Nombre del álbum 1</h3>
                   <button>Ver detalles</button>
@@ -30,10 +30,10 @@
               </figure>
             </div>
           </div>
-          <div class="column is-4">
-            <div class="gallery-item box" data-index="">
-              <figure class="image is-square">
-                <img src="https://images.unsplash.com/photo-1689851709772-aa131deb6e2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="" />
+          <div class="column">
+            <div class="gallery-item " data-index="">
+              <figure class="image">
+                <img src="../../img/neom-w-yuRFSkbVw-unsplash.jpg" alt="" />
                 <div class="album-info">
                   <h3>Nombre del álbum 1</h3>
                   <button>Ver detalles</button>
@@ -41,9 +41,9 @@
               </figure>
             </div>
           </div>
-          <div class="column is-4">
-            <div class="gallery-item box" data-index="">
-              <figure class="image is-square">
+          <div class="column">
+            <div class="gallery-item " data-index="">
+              <figure class="image">
                 <img src="https://images.unsplash.com/photo-1689192092151-5d8984fdd21d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="" />
                 <div class="album-info">
                   <h3>Nombre del álbum 1</h3>
@@ -53,9 +53,35 @@
               
             </div>
           </div>
-          <div class="column is-4">
-            <div class="gallery-item box" data-index="">
-              <figure class="image is-square">
+          <div class="column">
+            <div class="gallery-item " data-index="">
+              <figure class="image ">
+                <img src="../../img/christopher-campbell-rDEOVtE7vOs-unsplash.jpg" alt="" />
+                <div class="album-info">
+                  <h3>Nombre del álbum 1</h3>
+                  <button>Ver detalles</button>
+                </div>
+              </figure>
+              
+            </div>
+          </div>
+
+          <div class="column">
+            <div class="gallery-item " data-index="">
+              <figure class="image ">
+                <img src="../../img/ahmed-LKSGP7Jqqbw-unsplash (1).jpg" alt="" />
+                <div class="album-info">
+                  <h3>Nombre del álbum 1</h3>
+                  <button>Ver detalles</button>
+                </div>
+              </figure>
+              
+            </div>
+          </div>
+
+          <div class="column">
+            <div class="gallery-item " data-index="">
+              <figure class="image ">
                 <img src="../../img/christopher-campbell-rDEOVtE7vOs-unsplash.jpg" alt="" />
                 <div class="album-info">
                   <h3>Nombre del álbum 1</h3>
@@ -68,6 +94,54 @@
       </div>
     </div>
   </section>
+
+<!-- 
+
+  <section class="section">
+    <div class="container is-fluid">
+        <div class="columns">
+            <div class="column">
+              <div class="gallery-item" data-index="">
+                <figure>
+                <img src="https://source.unsplash.com/800x1000/?nature,water" onclick="openModal();currentSlide(1)">
+                <img src="https://source.unsplash.com/800x550/?nature,water" onclick="openModal();currentSlide(4)">
+                
+              </div>
+
+            </div>
+            <div class="column">
+                <img src="https://source.unsplash.com/800x600/?nature,water" onclick="openModal();currentSlide(2)">
+                <img src="https://source.unsplash.com/800x650/?nature,water" onclick="openModal();currentSlide(5)">
+            </div>
+            <div class="column">
+                <img src="https://source.unsplash.com/800x900/?nature,water" onclick="openModal();currentSlide(3)">
+                <img src="https://source.unsplash.com/800x700/?nature,water" onclick="openModal();currentSlide(6)">
+            </div>
+        </div>
+
+</section>
+ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <style>
 
@@ -91,11 +165,11 @@ font-size: 2.6rem;
     cursor: pointer;
   }
 
-  .gallery-item img {
+  /* .gallery-item img {
     width: 100%;
     height: 100%;
     object-fit: cover;
-  }
+  } */
 
 
 
