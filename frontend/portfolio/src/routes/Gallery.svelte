d7b72c583edcebcef2a9c5758ba65eba757fdca6
victor <victorl_222@hotmail.com>
2023-08-03 17:54:53 -0300

    add styles lightgallery

--- frontend/portfolio/src/routes/Gallery.svelte
+++ frontend/portfolio/src/routes/Gallery.svelte
@@ -4,9 +4,8 @@
 		console.log(e.detail.src)
 	}
 </script>
-<section class="section">
+<!-- <section class="section">
     <div class="container is-fluid">
-      <!-- <h2 class="title title-section">Photo</h2> -->
       <div class="columns ">
           <div class="column">
             <div class="gallery-item " data-index="">
@@ -93,9 +92,9 @@
           </div>
       </div>
     </div>
-  </section>
+  </section> -->
 
-<!-- 
+
 
   <section class="section">
     <div class="container is-fluid">
@@ -104,23 +103,57 @@
               <div class="gallery-item" data-index="">
                 <figure>
                 <img src="https://source.unsplash.com/800x1000/?nature,water" onclick="openModal();currentSlide(1)">
-                <img src="https://source.unsplash.com/800x550/?nature,water" onclick="openModal();currentSlide(4)">
-                
+                <div class="album-info">
+                  <h3>Nombre del álbum 1</h3>
+                  <button>Ver detalles</button>
+                </div>
+              </figure>
               </div>
 
             </div>
             <div class="column">
+              <div class="gallery-item" data-index="">
+                <figure>
                 <img src="https://source.unsplash.com/800x600/?nature,water" onclick="openModal();currentSlide(2)">
-                <img src="https://source.unsplash.com/800x650/?nature,water" onclick="openModal();currentSlide(5)">
+                <div class="album-info">
+                  <h3>Nombre del álbum 1</h3>
+                  <button>Ver detalles</button>
+                </div>
+              </figure>
             </div>
+          </div>
+
             <div class="column">
+              <div class="gallery-item">
+                <figure>
                 <img src="https://source.unsplash.com/800x900/?nature,water" onclick="openModal();currentSlide(3)">
-                <img src="https://source.unsplash.com/800x700/?nature,water" onclick="openModal();currentSlide(6)">
+                <div class="album-info">
+                  <h3>Nombre del álbum 1</h3>
+                  <button>Ver detalles</button>
+                </div>
+              </figure>
             </div>
+          </div>
+
+
+          <div class="column">
+            <div class="gallery-item">
+              <figure>
+              <img src="https://images.unsplash.com/photo-1690983177044-943a605d301c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=729&q=80" onclick="openModal();currentSlide(3)">
+              <div class="album-info">
+                <h3>Nombre del álbum 1</h3>
+                <button>Ver detalles</button>
+              </div>
+            </figure>
+          </div>
+        </div>
+
+
+        
         </div>
 
 </section>
- -->
+
 
 
 
@@ -163,32 +196,34 @@ font-size: 2.6rem;
 
     .gallery-item {
     cursor: pointer;
+    position: relative;
   }
 
-  /* .gallery-item img {
+   .gallery-item img {
     width: 100%;
-    height: 100%;
+    height: auto;
     object-fit: cover;
-  } */
+  } 
 
 
 
 
   .album-info {
     position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0, 0, 0, 0.7);
-    color: white;
-    opacity: 0; /* Ocultar por defecto, solo se mostrará al hacer hover */
-    pointer-events: none; /* Evitar que los elementos dentro de este div sean clickeables */
-    transition: opacity 0.3s;
+  bottom: 0; /* Colocar el texto y el botón en la parte inferior */
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px; /* Agregar un poco de espacio entre el texto y el botón */
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  opacity: 0;
+  pointer-events: none;
+  transition: opacity 0.3s;
   }
   .gallery-item:hover .album-info {
     opacity: 1;
